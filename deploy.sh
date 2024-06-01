9aa77e98a4c94029fb8bfe3ff6343f9ec453b9a8
Jack Curran <jackcurran49@gmail.com>
2024-06-01 11:26:00 -0400

    initial deployment resources

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,12 @@ if ! [ -d packages ]; then
     pip3 install -r requirements.txt -t packages/python/lib/python3.9/site-packages
 fi
 
-read -p "s3 bucket: " S3_BUCKET
+if ! [ -f s3_bucket.txt ]; then
+    read -p "s3 bucket: " bucket_name
+    echo $bucket_name > s3_bucket.txt
+fi
+
+read -r S3_BUCKET < s3_bucket.txt
 
 sam build -t cloudformation.yaml
 sam deploy -t cloudformation.yaml --stack-name GameAI --s3-bucket $S3_BUCKET
